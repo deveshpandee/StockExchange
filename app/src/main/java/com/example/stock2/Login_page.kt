03c81ff3b489e668145af13ffcf866653d65170d
deveshpandee <devesh2051@gmail.com>
2023-03-10 12:54:32 +0530

    Project initiated

---
+++ app/src/main/java/com/example/stock2/Login_page.kt
@@ -0,0 +1,11 @@
+package com.example.stock2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+    }
+}
